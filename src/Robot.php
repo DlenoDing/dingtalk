ffe115db1366e4fc9dbe8468ea68a84d73542f0b
Dleno <dleno@126.com>
2023-03-06 20:36:55 +0800

    init

--- src/Robot.php
+++ src/Robot.php
@@ -150,7 +150,7 @@ class Robot
     public function text(string $text)
     {
         if ($this->checkFrequencyMsg($text)) {
-            $text .= "> ** 请求追踪：**" . Server::getTraceId();
+            $text .= PHP_EOL . "> ** 请求追踪：**" . Server::getTraceId();
             return $this->ding('Text', 'text', $text);
         }
         return false;
@@ -163,7 +163,7 @@ class Robot
     public function markdown(string $markdown)
     {
         if ($this->checkFrequencyMsg($markdown)) {
-            $markdown .= "> ** 请求追踪：**" . Server::getTraceId();
+            $markdown .= PHP_EOL . "> ** 请求追踪：**" . Server::getTraceId();
             return $this->ding('Markdown', 'markdown', $markdown);
         }
         return false;
